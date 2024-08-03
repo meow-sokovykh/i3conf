9fc9268756efe20a63c2cade9a30b14352b1f727
meow-sokovykh <meow@sokovykh.me>
2024-08-03 16:08:23 +0300

    country code near extip

--- scripts/extip.sh
+++ scripts/extip.sh
@@ -5,6 +5,7 @@ rnd_id=$(shuf -i 1-10 -n 1)
 rnd_char=$(echo "$special_chars" | cut -c $rnd_id)
 
 IP="$(wget http://ipinfo.io/ip -qO -)"
+COUNTRY="$(curl -s ipinfo.io/country)"
 status=$?
 
 if [[ "$IP" == "" ]]; then
@@ -22,8 +23,8 @@ if [[ "$(echo "$IP" | grep '<')" != "" ]]; then
 	echo -e "prov.block"
 	echo -e "#FF5722"
 else # everything is fine
-	echo -e "$IP [$rnd_char]"
-	echo -e "$IP [$rnd_char]"
+	echo -e "$IP [$COUNTRY] $rnd_char"
+	echo -e "$IP [$COUNTRY] $rnd_char"
 	echo -e "#4CAF50"
 fi
 fi
